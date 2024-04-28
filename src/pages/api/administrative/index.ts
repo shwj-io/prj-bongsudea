b01ce8daf516651158ae0f9fbb4fd303a849d6ab
rendarCP <tjddnr0217@gmail.com>
2024-04-29 00:31:23 +0900

    feat: 행정구역관련 api 완료

---
+++ src/pages/api/administrative/index.ts
@@ -0,0 +1,48 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import { createClient } from '@/utils/server';
+
+function groupByCity(data) {
+  let subCityId = 1; // Start the sub-city id from 1
+
+  return data.reduce((acc, currentValue) => {
+    let group = acc.find(g => g.city === currentValue.main_city);
+
+    if (!group) {
+      group = { city: currentValue.main_city, area: [] };
+      acc.push(group);
+    }
+
+    // Add sub_city with an id property
+    group.area.push({ id: subCityId++, city: currentValue.sub_city });
+
+    return acc;
+  }, []);
+}
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const { query } = req;
+  const supabase = createClient();
+  if (query?.city) {
+    try {
+      const { data, error } = await supabase
+        .from('administrative')
+        .select()
+        .eq('main_city', query.city);
+      const transformedData = groupByCity(data);
+      res.status(200).json({ data: transformedData });
+    } catch (err) {
+      res.status(400).json({ message: '잘못된 요청입니다.' });
+    }
+  } else {
+    try {
+      const { data, error } = await supabase.from('administrative').select();
+      const transformedData = groupByCity(data);
+
+      res.status(200).json({ data: transformedData, error: error });
+    } catch (err) {
+      res.status(400).json({ message: '잘못된 요청입니다.' });
+    }
+  }
+}
