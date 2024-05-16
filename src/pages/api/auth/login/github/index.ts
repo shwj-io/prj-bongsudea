4016d98d01f373da005ec7ee89597bc936d2815d
rendarCP <tjddnr0217@gmail.com>
2024-05-17 00:00:53 +0900

    fix: 리다이렉션 수정

--- src/pages/api/auth/login/github/index.ts
+++ src/pages/api/auth/login/github/index.ts
@@ -15,11 +15,13 @@ export default async function handler(
     });
 
     if (error) throw error;
-    res.status(200).json({
-      data: data,
-      message: '깃허브로그인 링크 발급이 완료되었습니다.',
-      status: 200,
-    });
+
+    res.redirect(data.url);
+    // res.status(200).json({
+    //   data: data,
+    //   message: '깃허브로그인 링크 발급이 완료되었습니다.',
+    //   status: 200,
+    // });
   } catch (error) {
     res
       .status(400)
--- src/pages/test/index.tsx
+++ src/pages/test/index.tsx
@@ -13,9 +13,14 @@ export default function TestPage() {
         console.log('err', err);
       });
   };
+
+  const handleClick = () => {
+    axios.get('http://localhost:3000/api/auth/login/google');
+  };
   return (
     <div>
       <button onClick={() => handleSubmit()}>리셋 버튼</button>
+      <button onClick={() => handleClick()}>구글로그인</button>
     </div>
   );
 }
