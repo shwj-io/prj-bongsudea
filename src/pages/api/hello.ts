70e2e91d0cb9324a0c781d431c15ab1ffdb6ac11
rendarCP <tjddnr0217@gmail.com>
2024-08-21 23:34:21 +0900

    fix: issues 페이지 사이즈 관련 버그 해결

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -1,8 +1,7 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import createClient from '@/utils/createClient';
+import axios from 'axios';
 import type { NextApiRequest, NextApiResponse } from 'next';
-// import { redirect } from 'next/dist/server/api-utils';
-import { redirect } from 'next/navigation';
+import testData from '../../backend/240821_copy.json';
 
 type Data = {
   name: string;
@@ -12,70 +11,24 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  const supabase = createClient(req, res);
-  console.log('req @@@@@@@@@@@@@@', req.cookies);
-  // supabase
-  //   .from('notes')
-  //   .select()
-  //   .then(res => {
-  //     console.log('res', res);
-  //   });
-
-  // supabase
-  //   .from('profiles')
-  //   .select(`type_id, user_type( id )`)
-  //   .then(res => {
-  //     console.log('res', res);
-  //   });
-  // const { data, error } = await supabase.from('profiles').select(
-  //   `
-  //     id,
-  //     username,
-  //     avatar_url,
-  //     email,
-  //     user_type (
-  //       id,
-  //       type_name,
-  //       type_role
-  //     )
-  //   `
-  // );
-  // const { data, error } = await supabase.from('user_type').select(
-  //   `
-  //     id,
-  //     type_name,
-  //     type_role,
-  //     profiles (
-  //       id,
-  //       type_id,
-  //       username,
-  //       email
-  //     )
-  //   `
-  // );
-
-  // const { data, error } = await supabase.auth.signInWithOAuth({
-  //   provider: 'github',
-  // });
-
-  // supabase.auth
-  //   .signInWithOAuth({
-  //     provider: 'github',
-  //     options: {
-  //       redirectTo: 'http://localhost:3000/api/user',
-  //     },
-  //   })
-  //   .then(response => {
-  //     // console.log('response', response);
-  //     res.redirect(response.data.url);
-  //     // const test = supabase.auth.getUser();
-  //     // supabase.auth.getUser().then(user => {
-  //     //   console.log('user @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@', user);
-  //     //   res.status(200).json({ message: user });
-  //     // });
-  //   })
-  //   .catch(error => {
-  //     res.status(500).json({ error: error.message });
-  //   });
-  res.status(200).json({ name: 'John Doe', data: data, error });
+  const { query } = req;
+  const data = testData.disasterSmsList.map(item => {
+    const areaItem = item.RCV_AREA_NM.split(',')[0];
+    const convert = areaItem.includes('전체')
+      ? areaItem.replace('전체', '')
+      : areaItem;
+    return {
+      ...item,
+      converArea: convert.trim(),
+    };
+  });
+  res.status(200).json({ data });
+  // try {
+  //   const data = await axios.get(
+  //     `http://localhost:3000/api/issue/check/coordinate/kakao?area=${query.area}`
+  //   );
+  //   res.status(200).json({ message: '데이터 조회 성공', data: data.data });
+  // } catch (err) {
+  //   res.status(400).json({ message: '잘못된 요청입니다.', error: err });
+  // }
 }
